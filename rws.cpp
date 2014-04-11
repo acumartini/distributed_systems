4872a7a7a5c532f7160eaa3700822ecf786a6f1a
Adam Martini <martini@cs.uoregon.edu>
2014-04-11 00:38:11 -0700

    still optimizing

--- rws.cpp
+++ rws.cpp
@@ -8,17 +8,26 @@
 #include <fstream>
 #include <string>
 #include <iostream>
-#include <sstream> 
+#include <algorithm>
+// #include <boost/algorithm/string.hpp>
+// #include <sstream> 
 #include <iterator>
 #include <vector>
 #include <unordered_map>
 #include <unordered_set>
 #include <exception>
+
 #include <time.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 typedef std::unordered_set<int> EdgeSet;
 typedef std::unordered_map<int, EdgeSet> NodeMap;
 typedef std::unordered_map<int, int> IndexMap;
+typedef std::pair<int, int> NodePair;
+typedef std::vector<double> CreditVec;
 
 
 // global network storage
@@ -26,71 +35,136 @@ typedef std::unordered_map<int, int> IndexMap;
 NodeMap nodemap;
 IndexMap imap;
 
+
+// std::vector<std::string> string_split(std::string s, const char delimiter) {
+//     size_t start=0;
+//     size_t end=s.find_first_of(delimiter);
+    
+//     std::vector<std::string> output;
+    
+//     while (end <= std::string::npos) {
+// 	    output.emplace_back(s.substr(start, end-start));
+
+// 	    if (end == std::string::npos) 
+// 	    	break;
+
+//     	start=end+1;
+//     	end = s.find_first_of(delimiter, start);
+//     }
+    
+//     return output;
+// }
+
+int get_nodes(const std::string &line, NodePair &nodes) {
+	std::string source, target;
+	int i = 0, j = 0;
+	char c = line[j];
+
+	// eat white space
+	while (!isdigit(c)) { c = line[++j]; }
+	i = j;
+	// get source node
+	while (isdigit(c)) { c = line[++j]; }
+	source = line.substr(i, j);
+	i = j;
+	// eat white space
+	while (!isdigit(c)) { c = line[++j]; }
+	i = j;
+	// get target node
+	while (isdigit(c)) { c = line[++j]; }
+	target = line.substr(i, j);
+
+	// std::cout << "source: " << source << std::endl;
+	// std::cout << "target: " << target << std::endl;
+
+	// test for empty strings
+	if (source.length() == 0 || target.length() == 0) { return -1; }
+
+	// convert node string to integers
+	nodes.first = stoi(source);
+	nodes.second = stoi(target);
+
+	return 0;
+}
+
 /*
  * Reads the network from the given file into the global adjecency list unordered map.
  */
 void load_network(std::string filename) {
 	std::string line;
 	std::ifstream infile(filename);
+	NodePair nodes;
 	std::vector<int>::size_type sz;
 	EdgeSet edges;
-	int node_count = -1;
+	int line_count = -1;
 
 	if (infile) {
 		// get the line entry
 		while (getline(infile, line, '\n')) {
+			std::cout << "line#" << ++line_count << " : " << line <<std::endl; 
+
 			// split the line into tokens to get the kv pair
-    		std::istringstream buf(line);
-		    std::istream_iterator<std::string> beg(buf), end;
-		    std::vector<std::string> tokens(beg, end); // tokenized!
+    		// std::istringstream buf(line);
+		    // std::istream_iterator<std::string> beg(buf), end;
+		    // std::vector<std::string> tokens(beg, end); // tokenized!
+		    // boost::split(tokens, line, boost::is_any_of(' '));
 
 		    // validate token input size and update nodemap
-		    sz = tokens.size();
-		    if (sz == 2) {
+		    // sz = tokens.size();
+		    if (get_nodes(line, nodes) != -1) {
 				// convert key to integer
-				int node = stoi(tokens[0]);
-				int conn = stoi(tokens[1]);
+				int source = nodes.first;
+				int target = nodes.second;
 
-				// check for new node
-				if (imap.count(node) == 0) {
-					std::cout << "New node: " << node << '\n';
-					++node_count;
-					imap[node] = node_count;
+				// verify valid edge
+				if (source != target) {
+					// check if source node is new and update imap
+					// if (imap.count(source) == 0) {
+					// 	std::cout << "New node: " << source << '\n';
+					// 	++node_count;
+					// 	imap[source] = node_count;
+					// }
+
+					// // check if target node is new and update imap
+					// if (imap.count(target) == 0) {
+					// 	std::cout << "New node: " << target << '\n';
+					// 	++node_count;
+					// 	imap[target] = node_count;
+					// }
+
+					// update nodemap with new (undirected) edge
+					edges = nodemap[source];
+					edges.insert(target);
+					nodemap[source] = edges;
+
+					edges = nodemap[target];
+					edges.insert(source);
+					nodemap[target] = edges;
 				}
-
-				// update nodemap with new edge
-				edges = nodemap[node];
-				edges.insert(conn);
-				nodemap[node] = edges;
-
-				// check for membership and add edge to nodemap according
-				// if (nodemap.count(node) == 0) {
-				// 	vp = nodemap[node];
-				//     cur_index = vp.first;
-				//     edges = vp.second;
-
-				    // edges.insert(conn); // record edge
-				    // nodemap[node] = edges;
-				// } else {
-				    // std::cout << e.what() << '\n';
-				    // std::cout << "New node: creating new vector to store edge values." << '\n';
-				    // // edges = new std::unordered_set<int>();
-				    // ++node_count;
-				    // cur_index = node_count;
-
-				    // edges.clear();
-				    // edges.insert(conn);
-				    // nodemap[node] = edges;
-				// }
-				// edges.insert(conn); // record edge
-				// nodemap[node] = ValuePair(cur_index, edges);
-				
 			} else {
 				std::cout << "Input Error: 2 tokens per line expected." << std::endl;
 			}
    		}
+   		// populate imap
+   		int i = -1;
+   		for (auto& kv: nodemap) {
+   			imap[kv.first] = ++i;
+   		}
 	}
 	infile.close( );
+}
+
+/*
+ * Normalizes CreditVec C in place.
+ */
+void normalize(CreditVec &C) {
+	double sum = 0;
+	for (auto& credit: C) {
+		sum += credit;
+	}
+	for (int i=0; i<C.size(); ++i) {
+		C[i] /= sum;
+	}
 }
 
 /*
@@ -100,16 +174,26 @@ void load_network(std::string filename) {
  * @params: C - stores the current credit values for each node in the network
  * 			C_ - stores the credit values for each node at time step t+1
  */
-void compute_credit(std::vector<double> &C, std::vector<double> &C_) {
+void compute_credit(CreditVec &C, CreditVec &C_) {
 	double sum;
+	int node, i;
 
-	// for (int i=1; i<=C_.size(); ++i) {
-	// 	sum = 0;
-	// 	for (auto& edge_node: nodemap[i]) {
-	// 		sum += double (C[edge_node]) / double ((nodemap[edge_node].size()));
-	// 	}
-	// 	std::cout << sum << std::endl;
-	// }
+	for (auto& kv: nodemap) {
+		sum = 0;
+		for (auto& edge: kv.second) {
+			i = imap[edge];
+			// std::cout << "edge: " << edge << std::endl;
+			// std::cout << "credit update: " << C[i] / nodemap[edge].size() << std::endl;
+			sum += C[i] / nodemap[edge].size();
+		}
+		// std::cout << sum << std::endl;
+		i = imap[kv.first];
+		C_[i] = sum;
+	}
+
+	// noramlize credit vector
+	normalize(C_);
+	C = C_;
 }
 
 int main( int argc , char** argv ) {
@@ -129,27 +213,34 @@ int main( int argc , char** argv ) {
     std::cout << "time: " << seconds << std::endl;
 
 	// check hash
-	std::cout << "nodemap contains:" << std::endl;
-	for (auto& kv: nodemap) {
-		std::cout << kv.first << ": ";
-		EdgeSet es = kv.second;
-		std::cout << "index=" << imap[kv.first] << " size=" << es.size() << std::endl;
-		for (auto& edge: es) {
-			std::cout << edge << ", ";
-		}
-		std::cout << std::endl;
-	}
+	// std::cout << "nodemap contains:" << std::endl;
+	// for (auto& kv: nodemap) {
+	// 	std::cout << kv.first << ": ";
+	// 	EdgeSet es = kv.second;
+	// 	std::cout << "index=" << imap[kv.first] << " size=" << es.size() << std::endl;
+	// 	for (auto& edge: es) {
+	// 		std::cout << edge << " ";
+	// 	}
+	// 	std::cout << std::endl;
+	// }
 	
 	// compute the normalized credit after numSteps
 	std::cout << std::endl;
 	std::cout << "Computing the normalized credit vector after " << numSteps << " steps...";
 	std::cout << std::endl;
-	std::vector<double> credit(nodemap.size(), 1); // initialize credit at t=0 to 1 for each node
-	std::vector<double> credit_(nodemap.size(), 0);
+	CreditVec C(nodemap.size(), 1); // initialize credit at t=0 to 1 for each node
+	CreditVec C_(nodemap.size(), 0);
+
 	for (int i=0; i<numSteps; ++i) {
 		std::cout << "step : " << i << std::endl;
-		compute_credit(credit, credit_);
+		compute_credit(C, C_);
 	}
+
+	// check final credit
+	for (auto& credit: C) {
+		std::cout << credit << std::endl;
+	}
+	std::cout << "C.size()=" << C.size() << std::endl;
 
 	return 0 ;
 }
